11a104a9108f6e78848acd24b78a34ba61062b8a
Joseph-Pena <penajoseph270@gmail.com>
2026-03-25 00:40:35 -0400

    React Tv

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,33 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails.jsx";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null)
+
+  if(!show) {
+    return (
+      <section className="show-details">
+        <p>Please select a show to learn more.</p>
+      </section>
+      );
+    }
+
+  return (
+    <><section className="show-details">
+      <h2>{show.name}</h2>
+      <h3>{show.genre}</h3>
+    </section><div className="show-details">
+        <EpisodeList 
+          episodes={show.episodes}
+          selectedEpisode={selectedEpisode}
+          setSelectedEpisode={setSelectedEpisode}
+        />
+        <EpisodeDetails
+          episode={selectedEpisode} 
+        />
+      </div></>
+  )
 }
